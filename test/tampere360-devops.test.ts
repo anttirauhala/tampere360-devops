bf278b9e60abdda7f5a0160b33447333f22e0ff8
anttirauhala <anttirauhala@users.noreply.github.com>
2025-01-05 11:00:44 +0200

    cdk init executed

---
+++ test/tampere360-devops.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Tampere360Devops from '../lib/tampere360-devops-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tampere360-devops-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Tampere360Devops.Tampere360DevopsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
